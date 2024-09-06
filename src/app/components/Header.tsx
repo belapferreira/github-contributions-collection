0f9df37e49fa17b4a497056865d6044c3d0cb22d
belapferreira <isabelapenhaferreira@gmail.com>
2024-09-06 08:34:02 -0300

    chore: update the name in the header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,9 +2,7 @@ export const Header = () => {
   return (
     <header className="flex w-full items-center justify-center bg-gray-950 p-16">
       <div className="max-w-content-w flex w-full justify-center">
-        <h1 className="text-blue-light text-2xl font-bold">
-          GitHub Contributions
-        </h1>
+        <h1 className="text-blue-light text-2xl font-bold">Git Contribs</h1>
       </div>
     </header>
   );
