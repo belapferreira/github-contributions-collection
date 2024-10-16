903f81b6eecf9ea2f515fc6a8620a9d72a9a672a
belapferreira <isabelapenhaferreira@gmail.com>
2024-10-16 13:04:34 -0300

    chore: add generate static params

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
     GRAPHQL_GITHUB_API_URL: process.env.GRAPHQL_GITHUB_API_URL,
     TOKEN_GITHUB: process.env.TOKEN_GITHUB,
   },
-  output: 'export',
 };
 
 export default nextConfig;
