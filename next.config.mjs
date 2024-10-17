07be858c080c48dfb70d11a79e1eb5a6845ae16a
belapferreira <isabelapenhaferreira@gmail.com>
2024-10-17 08:31:09 -0300

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_GITHUB_GRAPHQL_API_URL:
-      process.env.NEXT_PUBLIC_GITHUB_GRAPHQL_API_URL,
-    NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
